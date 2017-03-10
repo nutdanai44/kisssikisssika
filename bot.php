1125cde280f5cca2eafcd582d5995602eb836781
nutdanai44 <mellonb77@gmail.com>
2017-03-11 05:56:34 +0700

    test

--- bot.php
+++ bot.php
@@ -44,7 +44,7 @@
                 $messages = [
                 'type' => 'text',
                 'text' => $text
-                ]
+                ];
                 
                 // Make a POST Request to Messaging API to reply to sender
                 $url = 'https://api.line.me/v2/bot/message/reply';
