551a62e882f3c2f54ab3f8a7e327c103f68c3f90
nutdanai44 <mellonb77@gmail.com>
2017-03-11 01:40:30 +0700

    test

--- bot.php
+++ bot.php
@@ -1,17 +1,20 @@
 <?php
     
+    require_once __DIR__ . "/vender/autoload.php";
 //    $access_token = "http://kisssikisssika.herokuapp.com/config.php";
     $access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
     $channelSecret = '6ff3fa91e07dac67e9088b03b2486981'
     
     
-    $bot = new BOT_API($channelSecret, $access_token);
+    $content = file_get_contents('php://input');
+    // Parse JSON
+    $events = json_decode($content, true);
+    $replyToken = $event['replyToken'];
     
-    $bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
-    if ($bot->isSuccess()) {
-        echo 'Succeeded!';
-        exit();
-    }
-    // Failed
-    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
-    exit();
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+    
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+    $response = $bot->pushMessage('<to>', $textMessageBuilder);
+    
+    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
