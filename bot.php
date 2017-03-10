05274306983b2cb0aff5a989b7153780ade65441
nutdanai44 <mellonb77@gmail.com>
2017-03-11 03:54:45 +0700

    test

--- bot.php
+++ bot.php
@@ -56,8 +56,9 @@
                 // Make a POST Request to Messaging API to reply to sender
                 $url = 'https://api.line.me/v2/bot/message/reply';
                 $data = [
-                'replyToken' => $replyToken,
-                'messages' => [$messages],
+//                'replyToken' => $replyToken,
+//                'messages' => [$messages],
+                $messages
                 ];
                 $post = json_encode($data);
                 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
