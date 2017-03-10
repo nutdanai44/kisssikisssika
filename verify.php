5a3d1caf442d00224a41a6aadb447d9c42a00801
nutdanai44 <mellonb77@gmail.com>
2017-03-11 00:32:50 +0700

    composer

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
     
-    $access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
-
+$access_token = "http://kisssikisssika.herokuapp.com/config.php";
+    
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
