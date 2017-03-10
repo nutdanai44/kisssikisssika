c4ef21a66f8c264a8d518f32a317c81ef2afa8bc
nutdanai44 <mellonb77@gmail.com>
2017-03-11 01:32:19 +0700

    test

--- bot.php
+++ bot.php
@@ -1,67 +1,17 @@
 <?php
     
-    include ('vendor/autoload.php');
-    use \LINE\LINEBot;
-    use \LINE\LINEBot\HTTPClient;
-    use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-    use \LINE\LINEBot\MessageBuilder;
-    use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-    
 //    $access_token = "http://kisssikisssika.herokuapp.com/config.php";
     $access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
-    $uId = '6ff3fa91e07dac67e9088b03b2486981'
+    $channelSecret = '6ff3fa91e07dac67e9088b03b2486981'
     
-////    // Get POST body content
-    $content = file_get_contents('php://input');
-////    // Parse JSON
-    $events = json_decode($content, true);
-//    // Validate parsed JSON data
-//    if (!is_null($events['events'])) {
-//        // Loop through each event
-//        foreach ($events['events'] as $event) {
-//            // Reply only when message sent is in 'text' format
-//            if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-//                // Get text sent
-//                $text = $event['message']['text'];
-//                // Get replyToken
-                $replyToken = $event['replyToken'];
-//
-//                // Build message to reply back
-//                $messages = [
-//                'type' => 'text',
-//                'text' => $text
-//                ];
-//                
-//                // Make a POST Request to Messaging API to reply to sender
-//                $url = 'https://api.line.me/v2/bot/message/reply';
-//                $data = [
-//                'replyToken' => $replyToken,
-//                'messages' => [$messages],
-//                ];
-//                $post = json_encode($data);
-//                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-//                
-//                $ch = curl_init($url);
-//                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-//                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-//                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-//                $result = curl_exec($ch);
-//                curl_close($ch);
-//                
-//                echo $result . "\r\n";
-//            }
-//        }
-//    }
     
-    $path = __DIR__ . '/vendor/autoload.php';
-    require_once $path;
-
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $uId]);
+    $bot = new BOT_API($channelSecret, $access_token);
     
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    $response = $bot->pushMessage($replyToken, $textMessageBuilder);
-    
-    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+    $bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
+    exit();
