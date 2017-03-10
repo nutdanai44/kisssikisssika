e9717c7f60b95bf23360fbda8d72e35572d19a32
nutdanai44 <mellonb77@gmail.com>
2017-03-11 04:09:36 +0700

    test

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
     
-//    require_once("/vender/autoload.php");
+    require_once __DIR__ . '/../vendor/autoload.php';
 //    require_once __DIR__ . "/vender/autoload.php";
 //    $access_token = "http://kisssikisssika.herokuapp.com/config.php";
     $access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
