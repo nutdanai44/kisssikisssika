23d5bbefc6a58dfdeaaaa31ea463bab97158dcc7
nutdanai44 <mellonb77@gmail.com>
2017-03-11 02:52:55 +0700

    test

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
     
-$access_token = require_once("config.php");
+require_once("config.php");
 //$access_token = "http://kisssikisssika.herokuapp.com/config.php";
 //$access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
     
