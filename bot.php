dc8da2fa4b2e106cc242138f4c831cbd8885bf18
nutdanai44 <mellonb77@gmail.com>
2017-03-11 02:39:09 +0700

    test

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
     
-//    require_once "http://kisssikisssika.herokuapp.com/config.php";
+    require_once __DIR__ . "/vender/autoload.php";
 //    $access_token = "http://kisssikisssika.herokuapp.com/config.php";
     $access_token = 'NtUwOPOhEsXR2X/96S2sY25kI1ZB9Kf57jSWfEdApNs6nIzaNr1+Tb+O4tIfKxF28e0GolRIKIXWkLcL3FovLPSjheL6H6Ez+u0U9YLckFV+OX7T27DHqX5oJlilaK5/ou6fSviCF4GCj4wL9U5aCwdB04t89/1O/w1cDnyilFU=';
     $channelSecret = '6ff3fa91e07dac67e9088b03b2486981';
@@ -20,6 +20,7 @@
                 $text = $event['message']['text'];
                 // Get replyToken
                 $replyToken = $event['replyToken'];
+                
                 
                 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
                 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
