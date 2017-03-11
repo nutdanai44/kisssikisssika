8ad58439c2ded27809227e536c519a28caebd778
nutdanai44 <mellonb77@gmail.com>
2017-03-11 07:06:44 +0700

    test

--- bot.php
+++ bot.php
@@ -33,24 +33,36 @@
                 // Get text sent
                 $text = $event['message']['text'];
     
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hi');
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hi2');
                 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                 
             } else if (($event['type'] == 'message') && ($event['message']['type'] == 'sticker')) {
-                $columns = array();
-                $img_url = 'http://www.bktube.net/wp-content/uploads/2017/01/XXX003.jpg';
+//                $columns = array();
+//                $img_url = 'http://www.bktube.net/wp-content/uploads/2017/01/XXX003.jpg';
 //                for($i=0;$i<5;$i++)
 //                {
-                    $actions = array(
-                                     //                                         action
-                        new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('ปุ่ม1','a'),
-                        new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('ปุ่ม2','http://www.google.com')
-                                     );
-                    $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder('คอลั่ม1', 'คอลั่ม2', $img_url , $actions);
-                    $columns[] = $column;
+//                    $actions = array(
+//                                     //                                         action
+//                        new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('ปุ่ม1','a'),
+//                        new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('ปุ่ม2','http://www.google.com')
+//                                     );
+//                    $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder('คอลั่ม1', 'คอลั่ม2', $img_url , $actions);
+//                    $columns[] = $column;
 //                }
-                $carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-                $msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('ก็มิรู้สินะ', $carousel);
+//                $carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
+//                $msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('ก็มิรู้สินะ', $carousel);
+//                $response = $bot->replyMessage($replyToken,$msg);
+                $actions = array(
+                                 new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('aa','ss1'),
+                                 new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Google','http://www.google.com')
+//                                 ,
+//                                 new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('sssdd', 'page=3'),
+//                                 new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('sad', 'page=1')
+                                 );
+                
+                $img_url = 'http://www.bktube.net/wp-content/uploads/2017/01/XXX003.jpg';
+                $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder('ee','rr', $img_url, $actions);
+                $msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('uiuiui', $button);
                 $response = $bot->replyMessage($replyToken,$msg);
             } else {
                 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event['message']['type']);
